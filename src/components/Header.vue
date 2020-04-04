b051b4f03c161f34d16d3454120a3e0c0f2347cd
Vikas Adyar <vikas.adyar@intertrust.com>
2020-04-04 20:49:20 +0530

    Adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Quiz Time</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
+    <Header/>
+    {{ $store.state.count }}
+    <br/>
     {{ this.results.index }}
     <br/>
     {{ this.results.correct }} / {{ this.results.total }}
-    <Card v-if="results.index < 5" :cue=results.active v-on:next="doSomething($event)"/>
+    <br/>
+    <v-btn small @click.prevent=resetQuestion>Reset</v-btn>
+    <Card v-if="results.index < 10" :cue=results.active v-on:next="doSomething($event)"/>
     
     <v-card
     class="mx-auto"
@@ -19,8 +24,8 @@
   </v-card>
 
     <div>
-      <v-btn small @click.prevent="previousQuestion()">Previous</v-btn>
-      <v-btn small @click.prevent="nextQuestion()">Next</v-btn>
+      <v-btn small @click.prevent="previousQuestion()" :disabled="results.index == 0">Previous</v-btn>
+      <v-btn small @click.prevent="nextQuestion()" :disabled="results.index == 10">Next</v-btn>
     </div>
   </div>
 </template>
@@ -28,11 +33,13 @@
 <script>
 // @ is an alias to /src
 import Card from '@/components/Card.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
     Card,
+    Header
   },
   data: function() {
     return {
@@ -41,14 +48,12 @@ export default {
         index: 0,
         active: {},
         correct: 0,
-        total: 10,
-        class_active: ""
+        total: 10
       }
     }
   },
   methods: {
     doSomething: function(event) {
-      console.log("dosomething")
       if( event ) {
         this.results.correct++;
       }
@@ -63,9 +68,12 @@ export default {
         this.results.active = this.getAllOptions(json.results[0]);
         });
     },
+    resetQuestion() {
+      this.results.index = 0
+      this.results.active = this.getAllOptions(this.results.questions[this.results.index]);
+    },
     nextQuestion() {
       this.results.active = this.getAllOptions(this.results.questions[++this.results.index]);
-      console.log(this.results.active.question)
     },
     previousQuestion() {
       this.results.active = this.getAllOptions(this.results.questions[--this.results.index]);
@@ -74,7 +82,6 @@ export default {
       let ret = item;
       let options = [...item.incorrect_answers , item.correct_answer];
       ret.options = this.shuffle(options);
-      console.log(ret);
       return ret;
     },
     shuffle(a) {
